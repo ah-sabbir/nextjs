f540abdff1c5e3f1eaa582a0870b1d987ecc24b9
Sabbir Ahmmed <sabbir@asoftwareltd.com>
2021-06-26 09:32:22 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1> baker bhai </h1>
+    <div>
+          <div className="header">
+            <div className="logo-bar">
+              <p>fantasymotion</p>
+            </div>
+            <div className="navbar">
+              <ul>
+                <li><a href="http://">Home</a></li>
+                <li><a href="http://">About</a></li>
+                <li><a href="http://">Contact us</a></li>
+                <li><a href="http://">products</a></li>
+              </ul>
+            </div>
+          </div>
+        <div className="bubles">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className="container">
+
+          <img src="/images/banner.png" alt="img-banner" className="img-fluid"/>
+        </div>
     </div>
   )
 }
