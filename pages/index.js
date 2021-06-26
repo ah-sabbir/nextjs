e6c0bbb3a9e0119537842db11920b667d0a479cb
Sabbir Ahmmed <sabbir@asoftwareltd.com>
2021-06-26 09:46:27 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,16 @@ export default function Home() {
           <div></div>
           <div></div>
           <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
     </div>
   )
